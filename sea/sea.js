3d27f98f1cb90f962f748504077d3ddd94a6c4b0
ZekieI <jo81@outlook.fr>
2025-04-18 22:12:20 +0200

    sea

--- sea/sea.js
+++ sea/sea.js
@@ -7,7 +7,6 @@ fetch('files/my-data.zip')
   })
   .then(JSZip.loadAsync)
   .then(zip => {
-    output.innerHTML = `<p>Images in ZIP:</p>`;
     const imageContainer = document.createElement('div');
     imageContainer.style.display = 'flex';
     imageContainer.style.flexWrap = 'wrap';
@@ -22,11 +21,9 @@ fetch('files/my-data.zip')
       file.async('blob').then(blob => {
         const img = document.createElement('img');
         img.src = URL.createObjectURL(blob);
-        img.alt = file.name;
         img.style.maxWidth = '200px';
         img.style.height = 'auto';
         img.style.border = '1px solid #ccc';
-        img.title = file.name;
         imageContainer.appendChild(img);
       });
     });
